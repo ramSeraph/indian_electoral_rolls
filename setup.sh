0a3e4ed4b168b373b884bf6fd13c33285a87a023
ramSeraph <kandimalla.sreeram@gmail.com>
2024-08-02 23:13:33 +0530

    oops

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-git clone git@github.com:ramSeraph/indian_electoral_rolls.git
-git clone https://github.com/ramSeraph/indian_electoral_rolls.git
-cd indian_electoral_rolls/
+#git clone git@github.com:ramSeraph/indian_electoral_rolls.git
+#git clone https://github.com/ramSeraph/indian_electoral_rolls.git
+#cd indian_electoral_rolls/
 mkdir data
 chmod 777 data/
 sudo mount -o discard,defaults /dev/sdb data/
 sudo chmod 777 data/
 
 sudo apt-get update
-sudo apt install tesseract-ocr python-is-python3 python3-venv ffmpeg libsm6 libxext6 libgl1-mesa-glx jq webp
+sudo apt install -y tesseract-ocr python-is-python3 python3-venv ffmpeg libsm6 libxext6 libgl1-mesa-glx jq webp
 
 python -m venv .venv
 source .venv/bin/activate
